a0219fc7ada17751ff5a7975d4cfeb633b6b83d4
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-25 15:24:49 +0530

    CSS Modifications

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,8 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // console.log('In another middleware');
@@ -14,9 +16,12 @@ router.get('/add-product', (req, res, next) => {
 });
 
 // /admin/add-product => POST
-router.post('/product', (req, res, next) => {
-  console.log(req.body);
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title })
   res.redirect('/');
 });
 
-module.exports = router;
+module.exports = {
+  routes: router,
+  products: products
+};
