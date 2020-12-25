4d190e750659de897f30e4da60b729a4db148a4a
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-25 13:43:54 +0530

    added body-parser dependency

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs');
-  next();
-});
+app.use(bodyParser.urlencoded());
 
 app.use('/add-product', (req, res, next) => {
   console.log('In another middleware');
