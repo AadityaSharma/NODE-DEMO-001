a917e7deaa8573b16834cce820131c70b9c4f583
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-25 14:43:13 +0530

    using a helper function (util) for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 // /admin/add-product => GET
@@ -8,7 +10,7 @@ router.get('/add-product', (req, res, next) => {
   // console.log('In another middleware');
   // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>');
 
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
