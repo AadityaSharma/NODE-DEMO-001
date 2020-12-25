a73c1a997beedd001e7de9520508b0dedf74c2dd
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-25 14:14:43 +0530

    Demofiltering paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   console.log('In another middleware');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>');
+  res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form>');
 });
 
+// /admin/add-product => POST
 router.post('/product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
