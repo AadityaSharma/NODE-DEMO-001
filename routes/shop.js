77cf5eafb48b4af1086a44108b125f53fe0a7294
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-27 09:32:05 +0530

    Adding the Layout (main-layout.hbs) to Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,15 @@ router.get('/', (req, res, next) => {
   // console.log('shop.js', adminData.products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   const products = adminData.products;
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProduct: products.length > 0});
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProduct: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+    // layout: false // setting false will not render the default layout
+  });
 });
 
 module.exports = router;
