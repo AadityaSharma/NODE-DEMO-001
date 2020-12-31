f22d6144bf7311a79ce692f87eb9f789f431ec0b
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-31 18:19:10 +0530

    Fetching data form files via the model

--- controllers/products.js
+++ controllers/products.js
@@ -22,14 +22,15 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProduct: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-    // layout: false // setting false will not render the default layout
+  Product.fetchAll((products) => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProduct: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+      // layout: false // setting false will not render the default layout
+    });
   });
 };
