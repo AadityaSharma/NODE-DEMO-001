8765e2241f17a14b9cae37691a08cf5abf40283c
Aaditya Sharma <accolite-asharma@evernote.com>
2021-01-12 23:38:26 +0530

    one-to-many and many-to-many relations

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -42,6 +44,10 @@ Product.belongsTo(User, {
   onDelete: 'CASCADE'
 });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 // sync() will create appropriate tables
 sequelize
