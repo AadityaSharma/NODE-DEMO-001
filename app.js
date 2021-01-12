398368df16acd84134e43fbbc1262a7f61900046
Aaditya Sharma <accolite-asharma@evernote.com>
2021-01-12 20:19:02 +0530

    Setup mysql

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 
+const db = require('./util/database');
+
+// db.end();
+db.execute('SELECT * FROM products')
+  .then(result => {
+    console.log(result[0], result[1]);
+  }).catch(err => {
+    console.log(err);
+  });
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
