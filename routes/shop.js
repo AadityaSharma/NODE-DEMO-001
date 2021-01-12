101b027250f2c560d56d0b3d7d6b5f28e8aaded6
Aaditya Sharma <accolite-asharma@evernote.com>
2021-01-13 00:46:19 +0530

    storing cartItems as orderItems

--- routes/shop.js
+++ routes/shop.js
@@ -20,6 +20,8 @@ router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
 
 router.get('/checkout', shopController.getCheckout);
+
+router.post('/create-order', shopController.postOrder);
 router.get('/orders', shopController.getOrders);
 
 module.exports = router;
