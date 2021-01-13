f97ee451e12305fcf3fe5fd3da8030818d44180f
Aaditya Sharma <accolite-asharma@evernote.com>
2021-01-13 19:42:29 +0530

    Installing the MongoDB Driver

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://aadiUserAdmin:699zZESpPArbx9D@cluster0.6tkr2.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  )
+    .then(result => {
+      console.log('Connected!');
+      callback(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
